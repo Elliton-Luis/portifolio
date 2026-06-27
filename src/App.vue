79b9aee87aec87ecc292c91d2db75a0c21dfa54c
Elliton-Luis <ellitonjunior05@gmail.com>
2026-06-27 13:10:51 -0300

    feat: add main application layout and sections

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
 <script setup>
-
+import Layout from './layout/Layout.vue';
+import HeroSection from './components/sections/HeroSection.vue';
+import SkillsSection from './components/sections/SkillsSection.vue';
+import ExperienceSection from './components/sections/ExperienceSection.vue';
+import ProjectsSection from './components/sections/ProjectsSection.vue';
+import AboutSection from './components/sections/AboutSection.vue';
 </script>
 
 <template>
-    <RouterView />
+  <Layout>
+    <HeroSection />
+    <SkillsSection />
+    <ExperienceSection />
+    <ProjectsSection />
+    <AboutSection />
+  </Layout>
 </template>
